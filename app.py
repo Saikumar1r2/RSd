b9886c487f7517b7d3194619bafdc2342393a5c8
Saikumar1r2 <Tothesaikumar@gmail.com>
2025-04-27 17:57:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ html_code = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Portfolio</title>
+    <title>My Portfo</title>
     <style>
         body {
             font-family: Arial, sans-serif;
